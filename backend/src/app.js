38d9fb44320e164d25be89836f8d4f3501e3232e
Gifty713 <giftj5584@gmail.com>
2026-08-13 15:08:44 +0100

    email notification and web socket connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { Server } from "socket.io";
 import {createServer} from "http";
 import cors from "cors";
+import {Resend} from "resend";
 import authRoute from "./routes/AuthRoutes.js";
 import workspaceRoute from "./routes/workspaceRoutes.js";
 import projectRoute from "./routes/projectRoutes.js";
@@ -21,11 +22,18 @@ app.use((req, res, next)=>{
 })
 
 io.on("connection", (socket)=>{
+    // link user id to socket
+    socket.on("authenticate",(userId)=>{
+        socket.userId = userId
+    });
     // connect the user to the project_room chat
     socket.on("join_room", (project_id)=>{
         socket.join(`roomId:${project_id}`)
     });
 });
+
+// resend config
+const resend = new Resend(process.env.RESEND_KEY);
 
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/workspace", workspaceRoute);
@@ -34,4 +42,4 @@ app.use("/api/v1/members", memberRoute);
 app.use("/api/v1/tasks", taskRoute);
 app.use("/api/v1/comments", commentRoute);
 
-export default app;
+export {app, resend};
--- backend/src/controllers/commentControllers.js
+++ backend/src/controllers/commentControllers.js
@@ -1,7 +1,8 @@
 import pool from "../config/database.js";
+import { resend } from "../app.js";
 const newComment=async(req, res)=>{
     try {
-        const {comment} = req.body;
+        const {comment, mentionId} = req.body;
         const sender = req.user;
         const project_id = req.params.project_id;
         // validate if this project is available
@@ -13,7 +14,46 @@ const newComment=async(req, res)=>{
             )  
         `,[project_id]);     
 
-        if(!foundProject.rows.exists) return(res.status(404).json({message:"Can't find this project, enter valid project id."})); 
+        if(!foundProject.rows[0].exists) return(res.status(404).json({message:"Can't find this project, enter valid project id."})); 
+        
+        // to check mentioned and send an email
+        if (mentionId){
+            // check if this is a valid user id in the particular project
+            const resultExists = await pool.query(`
+              SELECT EXISTS(
+                SELECT 1 
+                FROM members
+                WHERE user_id = $1 AND project_id = $2
+              )  
+            `,[mentionId, project_id]);
+            if (!resultExists.rows[0].exists) return res.status(404).json({message:"Couldn't find said user in this project."});
+            
+            // find user email
+            const resultUser = await pool.query(`
+              SELECT *
+              FROM users
+              WHERE id = $1
+            `, [mentionId]);
+            const resultEmail = resultUser.rows[0].email;
+
+            // if user not online in 1 hour then send email
+            setTimeout(async()=>{
+                const sockets = await req.io.fetchSockets();
+                const userIsOnline = sockets.some(
+                    socket => socket.userId === mentionId
+                );
+                if (!userIsOnline){
+                    const {data, error} = await resend.emails.send({
+                        from:"---",
+                        to: [`${resultEmail}`],
+                        subject:"Somebody mentioned you."
+                    })
+                    if (error) {
+                        console.error("Error in sending email.", error);
+                    }
+                }
+            },3600000);
+        };
         // result
         const result = await pool.query(`
            INSERT INTO comments(comment, sender, project_parent)
@@ -21,7 +61,7 @@ const newComment=async(req, res)=>{
            RETURNING *
         `,[comment, sender, project_id]);
 
-        res.status(200).json({message:"Comment added successfully.", result: result.rows[1]});
+        res.status(200).json({message:"Comment added successfully.", result: result.rows[0]});
         // broadcast the message
         req.io.to(`roomId:${project_id}`).emit("commented",{
             comment,
@@ -43,7 +83,7 @@ const getComments=async(req, res)=>{
             `, [project_id]
         );
         if (result.rows.length == 0) return res.status(404).json({message:"Couldn't find comments."});
-        const data = result.rows[0];
+        const data = result.rows;
         res.status(200).json({message:"Comments gotten.", data});
     } catch (error) {
         res.status(500).json({message:"Internal server error, error in getting comments.", error:error.message});                       
